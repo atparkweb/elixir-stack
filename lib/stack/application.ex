78f3d3bcf0eb43d060fd1d507d648217946d155c
Andy Park <atpark333@gmail.com>
2019-12-06 10:03:20 +0900

    Add stash to supervisor tree

--- lib/stack/application.ex
+++ lib/stack/application.ex
@@ -1,12 +1,15 @@
 defmodule Stack.Application do
+  @moduledoc false
+
   use Application
   
   def start(_type, _args) do
     children = [
-      { Stack.Server, [1,2,3] }
+      {Stack.Stash, [1,2,3]},
+      {Stack.Server, nil}
     ]
     
-    opts = [ strategy: :one_for_one, name: Stack.Supervisor ]
+    opts = [ strategy: :rest_for_one, name: Stack.Supervisor ]
     Supervisor.start_link(children, opts)
   end
 end
